bb304a5157821f12a44dfcae08bd8f2d40a6f10b
Andrade <93043336+PipelineDev@users.noreply.github.com>
2023-08-26 10:35:59 -0500

    Updated FilterData component, adding more props to it

--- src/components/FilterData.jsx
+++ src/components/FilterData.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function FilterData({children}) {
+function FilterData({ filters, onFilter }) {
+  const [filterText, setFilterText] = useState('')
+  const [filterValue, setFilterValue] = useState('Filtrar')
+  const [filterType, setFilterType] = useState('text')
+  const handleFilter = (e) => {
+    filterValue === 'Filtrar' ? setFilterProps('ID', 'number') : ''
+    setFilterText(e.target.value)
+    onFilter({ text: filterText, filterValue: filterValue })
+
+  }
+  const setFilterProps = (text, type) => {
+    setFilterValue(text)
+    setFilterType(type)
+  }
+  const handleFilterValue = ({ text, type }) => {
+    setFilterProps(text, type)
+    setFilterText('')
+
+  }
+
   return (
-    <form action="" className='mb-5'>
-        <div className="container-fluid row">
-          <input className='col form-control me-3' type="text" name="search" id="search" placeholder='Ingrese datos...'/>
-          <button className='col-2 btn btn-primary me-3'>Filtrar</button>
-          {children}
-        </div>
 
-      </form>
+    <div className="container-fluid mb-5">
+      <div className="row">
+        <input onChange={handleFilter} className='col form-control' type={filterType} name="search" id="search" placeholder='Ingrese datos...' value={filterText} />
+        <div className="dropdown pe-0 col-2">
+          <button className='btn w-100 btn-primary dropdown-toggle' type="button" data-bs-toggle="dropdown" aria-expanded="false">{filterValue}</button>
+          <ul className="dropdown-menu">
+            {filters.map((value, index) => {
+              return <li key={index}><a onClick={() => handleFilterValue(value)} className="dropdown-item" href="#">{value.text}</a></li>
+            })}
+
+          </ul></div></div>
+
+
+
+    </div>
+
+
   )
 }
 
